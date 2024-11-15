b916bed82813320b6db34e8f6e7b3ea74078b025
António Lira Fernandes <alf@esmonserrate.org>
2024-11-15 11:26:06 +0000

    first commit

---
+++ .history/app/ControllerCarros_20241115110614.php
@@ -0,0 +1,48 @@
+<?php
+namespace app;
+use app\Database;
+
+
+class ControllerCarros {
+
+    private $conn;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+// Obter todos os carros
+public function getAll() {
+	try {
+		$query = "SELECT * FROM pedroribeiroCarros";
+		$stmt = $this->conn->prepare($query);
+		$stmt->execute();
+		$carros = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+		echo json_encode($carros);
+	} catch (\PDOException $e) {
+		echo json_encode(['msg' => 'Erro: ' . $e->getMessage(), 'status' => '500']);
+	}
+}
+
+   // Obter carro por ID
+   public function getById($id) {
+    try {
+        $query = "SELECT * FROM pedroribeiroCarros WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id, \PDO::PARAM_INT);
+        $stmt->execute();
+        $carro = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if ($carro) {
+            echo json_encode($carro);
+        } else {
+            echo json_encode(['msg' => 'Carro não encontrado', 'status' => '404']);
+        }
+    } catch (\PDOException $e) {
+        echo json_encode(['msg' => 'Erro: ' . $e->getMessage(), 'status' => '500']);
+    }
+}
+
+}
+?>
